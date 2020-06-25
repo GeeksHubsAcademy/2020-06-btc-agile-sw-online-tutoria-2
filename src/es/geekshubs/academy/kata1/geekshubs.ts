5eabd8526ed7a7ee69628eeb357db875bbd276c5
vicboma1 <victorbolinchesmarin@gmail.com>
2020-06-25 19:00:03 +0200

    refactors

--- src/es/geekshubs/academy/kata1/geekshubs.ts
+++ src/es/geekshubs/academy/kata1/geekshubs.ts
@@ -2,21 +2,24 @@ export function _method(value: number) : string {
     return "vicboma"+value
 }
 
-const SALTO_LINEA = "\n";
+const SALTO_LINEA = "\n"
+const FIZZ = "Fizz"
+const BUZZ = "Buzz"
+const FIZZBUZZ = FIZZ+BUZZ
+const ZERO = 0
+const TRES = 3
+const CINCO = 5
+const EMPTY = String("")
 
 export function fizzbuzz(it: number) : string {
-    var res = ""
+    var res = EMPTY
     for(var i = 1; i < it + 1 ; i++){
-        // modulo 3 y 5
-        if(i % 5 === 0 && i % 3 == 0)
-            res += "FizzBuzz"+SALTO_LINEA
-        // modulo 5 
-        else if(i % 5 === 0)
-            res += "Buzz"+SALTO_LINEA
-        // modulo 3
-        else if(i % 3 === 0 )
-            res += "Fizz"+SALTO_LINEA;  
-        // caso base
+        if(i % CINCO === ZERO && i % TRES  == ZERO)
+            res += FIZZBUZZ+SALTO_LINEA
+        else if(i % CINCO === ZERO)
+            res += BUZZ+SALTO_LINEA
+        else if(i % TRES === ZERO )
+            res += FIZZ+SALTO_LINEA
         else 
             res += i + SALTO_LINEA
     }

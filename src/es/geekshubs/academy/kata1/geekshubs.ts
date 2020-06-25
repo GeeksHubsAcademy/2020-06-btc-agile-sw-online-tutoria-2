ec2d0d1f7e9e5f394952e0994c19886bcbf728fb
vicboma1 <victorbolinchesmarin@gmail.com>
2020-06-25 18:41:06 +0200

    test multiplos de 3 en verde

--- src/es/geekshubs/academy/kata1/geekshubs.ts
+++ src/es/geekshubs/academy/kata1/geekshubs.ts
@@ -4,19 +4,17 @@ export function _method(value: number) : string {
 
 export function fizzbuzz(it: number) : string {
     var res = ""
-
     for(var i = 1; i < it+1 ; i++){
-
         // modulo 3
-
+        if(i % 3 === 0 )
+            res += "Fizz\n";
         // modulo 5 
 
         // modulo 3 y 5
 
         // caso base
-        res += i +"\n"
-
+        else 
+            res += i +"\n"
     }
-
     return res;
 }

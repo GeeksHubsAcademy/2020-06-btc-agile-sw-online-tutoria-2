dd4878c5c75138f1e270bdf2718feabf88d13b81
vicboma1 <victorbolinchesmarin@gmail.com>
2020-06-25 18:55:14 +0200

    casos de multiplos 3 y 5 hasta 30

--- src/es/geekshubs/academy/kata1/geekshubs.ts
+++ src/es/geekshubs/academy/kata1/geekshubs.ts
@@ -6,7 +6,7 @@ const SALTO_LINEA = "\n";
 
 export function fizzbuzz(it: number) : string {
     var res = ""
-    for(var i = 1; i < it+1 ; i++){
+    for(var i = 1; i < it + 1 ; i++){
         // modulo 3 y 5
         if(i % 5 === 0 && i % 3 == 0)
             res += "FizzBuzz"+SALTO_LINEA
@@ -15,8 +15,7 @@ export function fizzbuzz(it: number) : string {
             res += "Buzz"+SALTO_LINEA
         // modulo 3
         else if(i % 3 === 0 )
-            res += "Fizz"+SALTO_LINEA;   
-        
+            res += "Fizz"+SALTO_LINEA;  
         // caso base
         else 
             res += i + SALTO_LINEA

b71d173222b9abdb9b78356fc0f9caa20463ce39
Xavier Venteo <xavierventeo@gmail.com>
2020-06-26 15:17:06 +0200

    Test múltipos de 3 en verde

--- src/es/geekshubs/academy/kata1/geekshubs.ts
+++ src/es/geekshubs/academy/kata1/geekshubs.ts
@@ -6,14 +6,17 @@ export function fizzBuzz(it: number) :string {
     var res = "";
     for (var i = 1; i <= it; i++) {
         // modulo 3
-
+        if (i % 3 === 0) {
+            res += "Fizz\n";
+        }
         // modulo 5
 
         // modulo 3 y 5
 
         // caso base
-        res += i + "\n";
-
+        else {
+            res += i + "\n";
+        }
     }
     return res;
 }

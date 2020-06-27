eb5b2b623ca28e8a9c560093a3dd0fdafabc0523
Xavier Venteo <xavierventeo@gmail.com>
2020-06-27 09:23:59 +0200

    Multiplos de 3 y 5 hasta 30

--- test/es/geekshubs/academy/kata1/suite.test.ts
+++ test/es/geekshubs/academy/kata1/suite.test.ts
@@ -38,7 +38,7 @@ describe('Testing', function (){
         expect(result).toBe(expected);
     });
 
-    test('Testeo números múltiplos de 3 y 5 para hacer FizzBuzz', function () {
+    test('Testeo números múltiplos de 3 y 5 para hacer FizzBuzz hasta 15', function () {
         //Arrange
         var expected = "1\n2\n\Fizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n";
         //Act
@@ -46,6 +46,16 @@ describe('Testing', function (){
         //Assert
         expect(result).toBe(expected);
     });
+
+    test('Testeo números múltiplos de 3 y 5 para hacer FizzBuzz hasta 30', function () {
+        //Arrange
+        var expected = "1\n2\n\Fizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n16\n17\nFizz\n19\nBuzz\nFizz\n22\n23\nFizz\nBuzz\n26\nFizz\n28\n29\nFizzBuzz\n";
+        //Act
+        var result = fizzBuzz(30);
+        //Assert
+        expect(result).toBe(expected);
+    });
+
 
 });
 

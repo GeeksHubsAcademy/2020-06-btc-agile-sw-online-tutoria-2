3e68f83e47128fb62ffb91d76bf22906e9610d83
Oscar Pérez <mr.omiguelperez@gmail.com>
2020-07-02 15:09:56 -0500

    Concat EOL to result ending each loop iteration

--- src/es/geekshubs/academy/kata1/geekshubs.ts
+++ src/es/geekshubs/academy/kata1/geekshubs.ts
@@ -7,18 +7,22 @@ const TRES = 3;
 const CINCO = 5;
 const CADENA_VACIA = "";
 
-export function fizzBuzz(it: number) :string {
-    var res = CADENA_VACIA;
-    for (var i = 1; i <= it; i++) {
+export function fizzBuzz(it: number): string {
+    let res = CADENA_VACIA;
+
+    for (let i = 1; i <= it; i++) {
         if ((i % TRES === ZERO) && (i % CINCO === ZERO)) {
-            res += FIZZBUZZ + SALTO_LINEA;
+            res += FIZZBUZZ;
         } else if (i % TRES === ZERO) {
-            res += FIZZ + SALTO_LINEA;
+            res += FIZZ;
         } else if (i % CINCO === ZERO) {
-            res += BUZZ + SALTO_LINEA;
+            res += BUZZ;
         } else {
-            res += i + SALTO_LINEA;
+            res += i;
         }
-       }
+
+        res += SALTO_LINEA;
+    }
+
     return res;
 }
